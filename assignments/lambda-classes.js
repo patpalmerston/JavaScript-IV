1a28e3ad01c2e4939c94f290244d71525aa4ae76
patpalmerston <patpalmerston@gmail.com>
2019-02-28 12:59:47 -0800

    MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -19,11 +19,11 @@ class Instructor extends Person {
     this.favLanguage = insAttrs.favLanguage;
     this.catchPhrase = insAttrs.catchPhrase;
   }
-  demo() {
+  demo(subject) {
     return `Today we are learning about ${subject}.`
   }
-  grade() {
-    return `${student.name} receives a perfect score on ${subject}.`
+  grade(student, subject) {
+    return `${student} receives a perfect score in ${subject}.`
   }
 }// Child
 
@@ -34,14 +34,14 @@ class Student extends Instructor {
     this.className  = stuAttrs.className ;
     this.favSubjects = stuAttrs.favSubjects;
   }
-  listsSubjects() {
-    return ` ${this.favSubjects}, ${this.favSubjects}, ${this.favSubjects}.`
+  listsSubjects(array) {
+    return array.forEach(x => console.log(x));
   }
-  PRAssignment() {
-    return `${student.name} has submitted a PR for ${subject}`
+  PRAssignment(student, subject) {
+    return `${student} has submitted a PR for ${subject}.`
   }
-  sprintChallenge() {
-    return `${student.name} has begun sprint challenge on ${subject}.`
+  sprintChallenge(student, subject) {
+    return `${student} has begun a sprint challenge on ${subject}.`
   }
 }// GrandChild
 
@@ -50,13 +50,13 @@ class ProjectManager extends Student {
     super(proAttrs)
     this.gradClassName  = proAttrs.gradClassName;
     this.favInstructor  = proAttrs.favInstructor ;
-  }
-  standUp() {
+  };
+  standUp(name, channel) {
     return `${name} announces to ${channel}, @channel standy times!`
-  }
-  debugsCode() {
-    return `${name} debugs ${student.name}'s code on ${subject}.`
-  }
+  };
+  debugsCode(name, student, subject) {
+    return `${name} debugs ${student}'s code on ${subject}.`
+  };
 }
 
 const professor1 = new Instructor({
@@ -69,6 +69,27 @@ const professor1 = new Instructor({
   catchPhrase: `Hard lessons learned are not soon forgotten.`
 });
 
+const professor2 = new Instructor({
+  name: 'Nancy',
+  location: 'Toledo',
+  age: 47,
+  gender: 'female',
+  favLanguage: 'React',
+  specialty: 'Front-end',
+  catchPhrase: `If you get stuck after 20 minutes, ask for help.`
+});
+
+const professor3 = new Instructor({
+  name: 'Zak',
+  location: 'Virginia',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'Less Compiler',
+  specialty: 'Front-end',
+  catchPhrase: `Make sure you take a break every 30 minutes.`
+});
+
+
 const student1 = new Student({
   name: 'Sarah',
   location: 'Ohio',
@@ -80,6 +101,28 @@ const student1 = new Student({
   catchPhrase: `I love learning.`
 });
 
+const student2 = new Student({
+  name: 'Alex',
+  location: 'California',
+  age: 23,
+  gender: 'male',
+  previousBackground: 'dog walker',
+  className: 'CS154',
+  favSubjects: ['React', 'Less', 'CSS'],
+  catchPhrase: `Sunshine is my friend.`
+});
+
+const student3 = new Student({
+  name: 'Tormond Dragonsbane',
+  location: 'Beyone the Wall',
+  age: 184,
+  gender: 'Non-Specific',
+  previousBackground: 'Underworld Ruler',
+  className: 'CS999',
+  favSubjects: ['Justice', 'Combat', 'Rainbows'],
+  catchPhrase: `From the darkness come the brightest of lights.`
+});
+
 const pm1 = new ProjectManager({
   name: 'Terry',
   location: 'Canada',
@@ -89,3 +132,35 @@ const pm1 = new ProjectManager({
   favInstructor: 'Jared',
   catchPhrase: `Helping makes me happy.`
 });
+
+const pm2 = new ProjectManager({
+  name: 'Mark',
+  location: 'New Mexico',
+  age: 22,
+  gender: 'male',
+  gradClassName: 'CS999',
+  favInstructor: 'Zak',
+  catchPhrase: `Sometimes I eat grasshoppers.`
+});
+
+const pm3 = new ProjectManager({
+  name: 'Alia',
+  location: 'Montana',
+  age: 27,
+  gender: 'female',
+  gradClassName: 'CS12',
+  favInstructor: 'Nancy',
+  catchPhrase: `Jumping in puddles is lots of fun.`
+});
+
+console.log(professor1.demo('Math'));
+console.log(professor1.grade(student1.name, 'English'));
+
+console.log(student1.listsSubjects(student1.favSubjects))
+console.log(student1.PRAssignment(student1.name, 'React'))
+console.log(student1.sprintChallenge(student1.name, 'React'))
+
+console.log(pm1.standUp(pm1.name, 'Web18'))
+console.log(pm1.debugsCode(pm1.name, student1.name, 'HTML'))
+
+
